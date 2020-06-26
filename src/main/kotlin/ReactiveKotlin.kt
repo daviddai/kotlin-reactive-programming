152482f93ba9cde20c5d99ed3158327b2bb18d1b
ddai <ddai@tripadvisor.com>
2020-06-26 18:55:08 +1000

    added sample of even odd number check in reactive way

--- src/main/kotlin/ReactiveKotlin.kt
+++ src/main/kotlin/ReactiveKotlin.kt
@@ -1,5 +1,7 @@
 import io.reactivex.Observable
 import io.reactivex.functions.BiFunction
+import io.reactivex.subjects.PublishSubject
+import io.reactivex.subjects.Subject
 
 fun main() {
     println("=== Print element by call fun that returns an Observable ===")
@@ -16,6 +18,12 @@ fun main() {
     println("=== Merged array by using mergeFlat ===")
     getObservableFromMergingLists(arrayOf(arrayOf("Java", "Kotlin"), arrayOf("Javascript")))
         .subscribe { mergedArray -> mergedArray.forEach { println(it) } }
+
+
+    println("=== Reactive Even or Odd ===")
+    val subject = getSubjectWithEvenOddCheck()
+    subject.onNext(10)
+    subject.onNext(11)
 }
 
 fun getObservableFromList(list: List<String>) = Observable.create<String> { emitter ->
@@ -39,3 +47,13 @@ fun getObservableFromMappingLists(items: Array<String>, colours: Array<String>):
 fun getObservableFromMergingLists(arrays: Array<Array<String>>): Observable<Array<String>> = Observable
     .fromArray(*arrays)
     .flatMap { array -> Observable.fromArray(array) }
+
+fun getSubjectWithEvenOddCheck(): Subject<Int> {
+    val subject: Subject<Int> = PublishSubject.create()
+
+    subject.map { isEven(it) }.subscribe { println("The number is ${if (it) "Even" else "Odd"}") }
+
+    return subject
+}
+
+fun isEven(number: Int) = number % 2 == 0
